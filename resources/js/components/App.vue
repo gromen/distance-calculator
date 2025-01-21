5a99b67874cb1b52de2b8a5867055859183c2f24
Marcin Gromek <gromson101@gmail.com>
2025-01-21 13:29:01 +0100

    chore: set up Vue.js with Vite configuration

---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container mx-auto p-4">
+        <h1 class="text-2xl font-bold mb-4">Distance Calculator</h1>
+        <p>Vue is working!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
