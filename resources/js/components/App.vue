06311e80abebf8930584774b269e6fbca8ff56b2
Marcin Gromek <gromson101@gmail.com>
2025-01-22 10:36:46 +0100

    chore: update project dependencies and improve configuration

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,24 @@
 <template>
-    <div class="container mx-auto p-4">
-        <h1 class="text-2xl font-bold mb-4">Distance Calculator</h1>
-        <p>Vue is working!</p>
-    </div>
+  <div class="container mx-auto p-4">
+    <h1 class="text-2xl font-bold mb-4">Distance Calculator</h1>
+    <p>Vue is working!</p>
+  </div>
 </template>
 
-<script>
-export default {
-    name: 'App'
-}
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'App',
+
+    data() {
+      return {
+        startLat: 0 as number,
+        startLng: 0 as number,
+        endLat: 0 as number,
+        endLng: 0 as number,
+        distance: null as number | null,
+      };
+    },
+  });
 </script>
