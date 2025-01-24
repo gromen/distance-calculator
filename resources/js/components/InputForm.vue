bfaa03c0131bc6c018fb540c20b8c7ab4c839cb6
Marcin Gromek <gromson101@gmail.com>
2025-01-24 15:20:33 +0100

    fix: update input precision in InputForm component

--- resources/js/components/InputForm.vue
+++ resources/js/components/InputForm.vue
@@ -10,7 +10,7 @@
         :name="id"
         :value="modelValue"
         @input="handleInput"
-        step="0.00000001"
+        step="0.1"
         :placeholder="placeholder"
         class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
       />
@@ -41,7 +41,6 @@
   }>();
 
   const showEmptyError = ref(false);
-
   const handleInput = (event: Event) => {
     const value = (event.target as HTMLInputElement).value;
     showEmptyError.value = value === '';
